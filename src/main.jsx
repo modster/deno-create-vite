e33069e2d9ced0590ee5954b56a1e3b4daa16114
E. M. Greeff <emgreeff@datadrivendashboards.com>
2023-03-10 05:57:46 -0700

    experimentalationism: charts

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import { React, ReactDOM } from "./deps.ts";
+import React from "./deps.ts";
+// import ReactDOM from "./deps.ts";
+import App from "./App.jsx";
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
